4bdf996d4faacbb04196061977162d231b127276
Hakan Arslan <128188577+Hkn36Arslan@users.noreply.github.com>
2024-12-23 09:40:19 +0300

    login sayfası oluşturuldu ve başarılı giriş sonrası admin panel sayfasına yönlendirme yapıldı.

--- webui/app.js
+++ webui/app.js
@@ -28,6 +28,30 @@ $(document).ready(function (event) {
       },
     });
   });
+  /* ****************************************** ADMİN LOGİN ******************************************* */
+
+  $("#adminLogin").on("submit", (e) => {
+    e.preventDefault();
+
+    console.log("username:", $("#username").val())
+    console.log("password:", $("#password").val())
+    console.log("typeOFusername:", typeof $("#username").val())
+    console.log("typeOFpassword:", typeof $("#password").val())
+
+    if ($("#username").val() === username && $("#password").val() === password) {
+      showAlert("success", `Login successfully ${userName}.`);
+      setTimeout(() => {
+        window.location.href = "admin.html";
+      }, 1500);
+    }
+    else {
+      showAlert(
+        "danger",
+        "Username or password is incorrect!"
+      );
+    }
+  });
+
   /* ******************************************* ADMİN PANEL ***************************************** */
   // Sunucudan başvuru bilgilerini al
   const getApplications = () => {
